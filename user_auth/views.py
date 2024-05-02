ea6169aa9882ef6a5f7e6c4cfbed17085759cf03
fodail2011 <106406115+fodail2011@users.noreply.github.com>
2024-05-02 08:45:35 +0300

    login

--- user_auth/views.py
+++ user_auth/views.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from yaml import serialize
 from .serializers import HospitalUserSerializer #PatientProfileSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 class RegisterStepOneAPIView(APIView):
@@ -44,7 +45,9 @@ class LoginAPIView(APIView):
         user = authenticate(username=username, password=password)
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response({
+                "user_id": user.id,
+                'token': token.key}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
